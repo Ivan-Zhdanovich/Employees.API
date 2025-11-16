0a4dab60a04183a188b0df9b3a019da713022617
Zhdanovich <Zhiv80@mail.ru>
2025-11-16 11:41:02 +0300

    "feat: add db context and packages, change employee entity, remove unnecessary classes "

--- src/Employees.Infrastructure/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Employees.Infrastructure;
-
-public class Class1
-{
-
-}
---
+++ src/Employees.Infrastructure/Persistence/EmployeesDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Employees.Domain.Entities
+
+namespace Employees.Infrastructure.Persistence;
+
+internal class EmployeesDbContext : DbContext
+{
+    internal DbSet<Employee> Employees { get; set; } 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        optionsBuilder.UseNpgsql("Host = localhost; Port = 5432; Database = EmploeesDb; Username = user; Password = password");
+        }
+}
