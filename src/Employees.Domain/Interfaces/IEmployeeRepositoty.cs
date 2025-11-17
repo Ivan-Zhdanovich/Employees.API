6bf867bc4fd70c37a831a0c929cd51c8fedc0f06
Zhdanovich <Zhiv80@mail.ru>
2025-11-17 17:24:37 +0300

    "feat: add coonectins string, change db context, add packages, add migration"

--- src/Employees.Domain/Interfaces/IEmployeeRepositoty.cs
+++ src/Employees.Domain/Interfaces/IEmployeeRepositoty.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Employees.Domain
+namespace Employees.Domain.Interfaces
 {
     internal interface IEmployeeRepository
     {
---
+++ src/Employees.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Employees.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Employees.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionsString = configuration.GetConnectionString("EmployeesDb");
+            services.AddDbContext<EmployeesDbContext>(options => options.UseNpgsql(connectionsString));
+          
+        }
+    }
+}
