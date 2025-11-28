ba198edaa414d2ad9b2063ad0a620ebcb5db2582
Zhdanovich <Zhiv80@mail.ru>
2025-11-28 12:28:27 +0300

    feat: add employee to user application relation, remove unneseccary migrations

--- src/Employees.Domain/Entities/ApplicationUser.cs
+++ src/Employees.Domain/Entities/ApplicationUser.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Employees.Domain.Entities
 {
       public class ApplicationUser : IdentityUser<int>
     {
+        public int EmployeeId { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public required Employee Employee { get; set; }
     }
 }
